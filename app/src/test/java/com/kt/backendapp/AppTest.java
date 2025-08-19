da9fd2a9cd885c665f4748d3da38475144d75811
김나영 <and___young@Nayoung-MacBookAir.local>
2025-08-19 14:34:53 +0900

    초기 설정 최종

--- app/src/test/java/com/kt/backendapp/AppTest.java
+++ app/src/test/java/com/kt/backendapp/AppTest.java
@@ -1,13 +1,13 @@
-package com.example.backend;
+package com.kt.backendapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class AppTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
